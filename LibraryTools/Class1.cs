7ec752d63d177783023fa096ee91bfb28016128d
Alberto Paulo <alberto.paulo@yukisoftware.es>
2021-11-05 12:56:55 +0100

    Singleton class Log and reference added

--- LibraryTools/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LibraryTools
-{
-	 public class Class1
-	 {
-	 }
-}
---
+++ LibraryTools/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace LibraryTools
+{
+	 public sealed class Log
+	 {
+		  private readonly static Log _instance = new Log();
+		  private string _path = "log.txt";
+
+		  public static Log Instance
+		  {
+				get
+				{
+					 return _instance;
+				}
+		  }
+
+		  private Log()
+		  {
+
+		  }
+
+		  public void Save(string logMessage) => File.AppendAllText(_path, logMessage + Environment.NewLine);
+	 }
+}
