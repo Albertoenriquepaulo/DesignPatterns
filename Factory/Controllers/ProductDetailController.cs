6b997216638cba06df943123dbd197faec15bece
Alberto Paulo <alberto.paulo@yukisoftware.es>
2021-11-11 19:21:07 +0100

    Completing

--- Factory/Controllers/ProductDetailController.cs
+++ Factory/Controllers/ProductDetailController.cs
@@ -9,12 +9,15 @@ namespace Factory.Controllers
 		  {
 			  // factories
 			  var localEarnFactory = new LocalEarnFactory(0.20m);
+			  var foreignEarnFactory = new ForeignEarnFactory(0.30m, 20m);
 			  
 			  // products
 			  var localEarn = localEarnFactory.GetEarn();
+			  var foreignEarn = foreignEarnFactory.GetEarn();
 			  
 			  // calculating earns
 			  ViewBag.totalLocal = total + localEarn.Earn(total);
+			  ViewBag.totalForeign = total + foreignEarn.Earn(total);
 			  
 			  return View();
 		  }
--- LibraryTools/Earn/ForeignEarn.cs
+++ LibraryTools/Earn/ForeignEarn.cs
@@ -1,7 +1,19 @@
 ﻿namespace LibraryTools.Earn
 {
-    public class ForeignEarn
+    public class ForeignEarn : IEarn
     {
-        
+        private readonly decimal _percentage;
+        private readonly decimal _extra;
+
+        public ForeignEarn(decimal percentage, decimal extra)
+        {
+            _percentage = percentage;
+            _extra = extra;
+        }
+
+        public decimal Earn(decimal amount)
+        {
+            return (amount * _percentage) + _extra;
+        }
     }
 }
